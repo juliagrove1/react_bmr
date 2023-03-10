867448c02b1d27be90377994817e4295091af4e1
juliagrove1 <112003183+juliagrove1@users.noreply.github.com>
2023-03-10 13:39:37 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import app from './app';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <app />
+    <App />
   </React.StrictMode>
 );
